728cc0a7fcb36c708b9791eae752217ae6d1ef56
Andrew Schmelyun <schmelyun@gmail.com>
2021-04-02 03:38:50 -0400

    Adds missing CategoryController include

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ItemController;
 
 Route::get('items', [ItemController::class, 'index']);
